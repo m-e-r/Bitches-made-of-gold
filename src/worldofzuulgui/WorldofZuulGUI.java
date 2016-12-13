8e4c9047477e2a4aefd7ebdb3783232d52434fc4
Øllebrød <3>
2016-12-13 15:13:36 +0100

    C for D

--- src/worldofzuulgui/WorldofZuulGUI.java
+++ src/worldofzuulgui/WorldofZuulGUI.java
@@ -9,8 +9,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,7 +19,7 @@ public class WorldofZuulGUI extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TitleScreen.fxml"));
 
         
         Scene scene = new Scene(root);
